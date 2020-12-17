5e805c5d3428ed59352d99240394435d690c9045
Teangelo Burks <burks.teangelo@gmail.com>
2020-12-17 15:18:09 -0500

    updated background, alert message once game is over, added more styling to CSS and updating Javascript

--- script.js
+++ script.js
@@ -1,64 +1,94 @@
-// first we will create two functions that will make the ball move left and right.
-
 var character = document.getElementById("character");
 var game = document.getElementById("game");
 var interval;
 var both = 0;
 var counter = 0;
+var currentBlocks = [];
 
-function moveLeft() {
-    var left =
-        parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    if (left > 0) { //This limits the ball from leaving the box it's inside of
+function moveLeft(){
+    var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    if(left>0){
         character.style.left = left - 2 + "px";
-
     }
 }
-
-function moveRight() {
-    var left =
-        parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    if (left < 380) {
+function moveRight(){
+    var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    if(left<380){
         character.style.left = left + 2 + "px";
     }
 }
-
 document.addEventListener("keydown", event => {
-    if (both == 0) {
-        both++
-
-        if (event.key === "ArrowLeft") {
+    if(both==0){
+        both++;
+        if(event.key==="ArrowLeft"){
             interval = setInterval(moveLeft, 1);
         }
-        if (event.key === "ArrowRight") {
-            interval = setInterval(moveRight, 1); //inside the if statements, we create intervals that will run the correct function.
-
+        if(event.key==="ArrowRight"){
+            interval = setInterval(moveRight, 1);
         }
     }
-
 });
-
 document.addEventListener("keyup", event => {
     clearInterval(interval);
-    both = 0;
+    both=0;
 });
 
-//now we're creating a block and a whole. we will add new elements for each.
-// we will also add classes so we can style them using CSS. 
-//created a interval function so that it keeps creating holes & blocks over and over again.
-
-setInterval(function () {
+var blocks = setInterval(function(){
     var blockLast = document.getElementById("block"+(counter-1));
-    var holeLast = document.getElementById("block"+(counter-1));
-    var block = document.createElement("div");
-    var hole = document.createElement("div");
-    block.setAttribute("class", "block")
-    hole.setAttribute("class", "hole");
-    block.setAttribute("id", "block");
-    hole.setAttribute("id", "hole");
-    var random = Math.floor(Math.random() * 360); // set to 360 to leave 20px's open to act as a whole. initial size is 380, this will randomize where the hole is placed.
-    hole.style.left = random + "px";
-    game.appendChild(block);
-    game.appendChild(hole);
-    counter++ // we created a counter to keep track of how many times the interval is run so we know how many blocks have been created
-}, 1);
+    var holeLast = document.getElementById("hole"+(counter-1));
+    if(counter>0){
+        var blockLastTop = parseInt(window.getComputedStyle(blockLast).getPropertyValue("top"));
+        var holeLastTop = parseInt(window.getComputedStyle(holeLast).getPropertyValue("top"));
+    }
+    if(blockLastTop<400||counter==0){
+        var block = document.createElement("div");
+        var hole = document.createElement("div");
+        block.setAttribute("class", "block");
+        hole.setAttribute("class", "hole");
+        block.setAttribute("id", "block"+counter);
+        hole.setAttribute("id", "hole"+counter);
+        block.style.top = blockLastTop + 100 + "px";
+        hole.style.top = holeLastTop + 100 + "px";
+        var random = Math.floor(Math.random() * 360);
+        hole.style.left = random + "px";
+        game.appendChild(block);
+        game.appendChild(hole);
+        currentBlocks.push(counter);
+        counter++;
+    }
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    var drop = 0;
+    if(characterTop <= 0){
+        alert("Game Over. Score: "+(counter-9));
+        clearInterval(blocks);
+        location.reload();
+    }
+    for(var i = 0; i < currentBlocks.length;i++){
+        let current = currentBlocks[i];
+        let iblock = document.getElementById("block"+current);
+        let ihole = document.getElementById("hole"+current);
+        let iblockTop = parseFloat(window.getComputedStyle(iblock).getPropertyValue("top"));
+        let iholeLeft = parseFloat(window.getComputedStyle(ihole).getPropertyValue("left"));
+        iblock.style.top = iblockTop - 0.5 + "px";
+        ihole.style.top = iblockTop - 0.5 + "px";
+        if(iblockTop < -20){
+            currentBlocks.shift();
+            iblock.remove();
+            ihole.remove();
+        }
+        if(iblockTop-20<characterTop && iblockTop>characterTop){
+            drop++;
+            if(iholeLeft<=characterLeft && iholeLeft+20>=characterLeft){
+                drop = 0;
+            }
+        }
+    }
+    if(drop==0){
+        if(characterTop < 480){
+            character.style.top = characterTop + 2 + "px";
+        }
+    }else{
+        character.style.top = characterTop - 0.5 + "px";
+    }
+},1);
